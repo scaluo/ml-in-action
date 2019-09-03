3436d9f11bb6852a2c91ba036e03c762ec5c80a4
liwenjie <scaluo@gmail.com>
2019-09-03 16:22:04 +0800

    over bayes

--- ch4/bayes.py
+++ ch4/bayes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 #创建训练数据集和标签数组
 def loadDataSet():
@@ -29,7 +30,18 @@ def setOfWords2Vec(vocabList,inputSet):
             print('the word:%s is not in vocablist!'%word)
     return returnVec
 
+#文档词袋模型
+def bagOfWords2VecMN(vocaList,imputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+        else:
+            print('the word:%s is not in vocablist!'%word)
+    return returnVec
+
 def trainNB0(trainMatrix,trainCategory):
+    
     #计算训练的文档数目
     numTrainDocs = len(trainMatrix)
     #计算每篇文档的词条数，这个传入的是已经标记过单词表的，每条记录的长度都是单词表的长度
@@ -62,8 +74,6 @@ def trainNB0(trainMatrix,trainCategory):
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) +np.log(pClass1)             #对应元素相乘
     p0 = sum(vec2Classify * p0Vec) +np.log(1.0 - pClass1)
-    print('p0:',p0)
-    print('p1:',p1)
     if p1 > p0:
         return 1
     else: 
@@ -80,5 +90,57 @@ def testingNB():
     thisDoc = np.array(setOfWords2Vec(vocabList,testEntry))
     print("classify is:%d"%classifyNB(thisDoc,p0V,p1V,pAb))
 
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W*',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok)>2]
+
+#邮件分类,spam,ham
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    #遍历两个目录，有侮辱性的和无侮辱性的，创建文档LIST和标签LIST,fullText没用到,原TXT文件是ANSI的，要用8859编码打开
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt'%i,encoding='ISO-8859-1').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt'%i,encoding='ISO-8859-1').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    #简历词汇表
+    vocabList = createVocabList(docList)
+    
+    
+    #用10条记录做测试集,书上的没运行通过，改成另一种方式
+    trainingSet = list(range(len(docList)))
+    testSet = []
+    iset = random.sample(trainingSet,10)
+    for i in iset:
+        testSet.append(i)
+        trainingSet.remove(i)
+    trainMat = []
+    trainClasses = []
+
+    #训练
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(np.array(trainMat),np.array(trainClasses))
+
+    #测试
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList,docList[docIndex])
+        if classifyNB(np.array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount+=1
+    print("The error rate is :%f"%(float(errorCount)/len(testSet)))
+
+
 if __name__=='__main__':
-    testingNB()
+    import chardet
+    #testingNB()
+    spamTest()
+    
