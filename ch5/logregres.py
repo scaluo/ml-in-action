24cb594c11c36b076ddf4695183951a5eb3d55a1
liwenjie <scaluo@gmail.com>
2019-09-04 14:40:59 +0800

    continue logregres

--- ch5/logregres.py
+++ ch5/logregres.py
@@ -2,65 +2,43 @@ from math import exp
 import numpy as np
 import matplotlib.pyplot as plt
 
+#创建数据集，返回特征数据和标签列
 def loadDataSet():
-    dataMat = []                                                        #创建数据列表
-    labelMat = []                                                       #创建标签列表
-    fr = open('testSet.txt')                                            #打开文件   
-    for line in fr.readlines():                                         #逐行读取
-        lineArr = line.strip().split()                                  #去回车，放入列表
-        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])     #添加数据
-        labelMat.append(int(lineArr[2]))                                #添加标签
-    fr.close()                                                          #关闭文件
-    return dataMat, labelMat                                            #返回
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    fr.close()
+    return dataMat, labelMat
 
-"""
-函数说明:sigmoid函数
-
-Parameters:
-    inX - 数据
-Returns:
-    sigmoid函数
-Author:
-    Jack Cui
-Blog:
-    http://blog.csdn.net/c406495762
-Zhihu:
-    https://www.zhihu.com/people/Jack--Cui/
-Modify:
-    2017-08-28
-"""
+#跃阶sigmoid函数
 def sigmoid(inX):
     return 1.0 / (1 + np.exp(-inX))
 
-"""
-函数说明:梯度上升算法
-
-Parameters:
-    dataMatIn - 数据集
-    classLabels - 数据标签
-Returns:
-    weights.getA() - 求得的权重数组(最优参数)
-Author:
-    Jack Cui
-Blog:
-    http://blog.csdn.net/c406495762
-Zhihu:
-    https://www.zhihu.com/people/Jack--Cui/
-Modify:
-    2017-08-28
-"""
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = np.mat(dataMatIn)                                      #转换成numpy的mat
-    labelMat = np.mat(classLabels).transpose()                          #转换成numpy的mat,并进行转置
-    m, n = np.shape(dataMatrix)                                         #返回dataMatrix的大小。m为行数,n为列数。
-    alpha = 0.001                                                       #移动步长,也就是学习速率,控制更新的幅度。
-    maxCycles = 500                                                     #最大迭代次数
+    #数组转换成numpy的mat
+    dataMatrix = np.mat(dataMatIn)
+    #标签列转换成numpy的mat,并进行转置
+    labelMat = np.mat(classLabels).transpose()
+    #返回dataMatrix的大小。m为行数,n为列数。
+    m, n = np.shape(dataMatrix)
+    #移动步长,也就是学习速率,控制更新的幅度。
+    alpha = 0.001
+    #最大迭代次数
+    maxCycles = 500
+    #初始化W是N*1，值为1的数组
     weights = np.ones((n,1))
     for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)                               #梯度上升矢量化公式
+        #梯度上升矢量化公式
+        h = sigmoid(dataMatrix * weights)
         error = labelMat - h
+        #w = w+a*d*e
         weights = weights + alpha * dataMatrix.transpose() * error
-    return weights.getA()                                               #将矩阵转换为数组，返回权重数组
+    #将矩阵转换为数组，返回权重数组
+    return weights.getA()
 
 def plotBestFit(wei):
     dataMat,labelMat = loadDataSet()
@@ -88,3 +66,7 @@ def plotBestFit(wei):
     plt.ylabel('X2')
     plt.show()
 
+if __name__ == '__main__':
+    dataMat,labels = loadDataSet()
+    wei = gradAscent(dataMat,labels)
+    plotBestFit(wei)
